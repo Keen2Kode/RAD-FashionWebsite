28ed38f796771af88b6afa14c8b2a26894db98bd
Ubuntu <ubuntu@ip-172-31-13-115.us-east-2.compute.internal>
2021-04-23 08:00:07 +0000

    Moved newsletter to partial but not posting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,8 @@ class HomeController < ApplicationController
     @random_item = Item.order('RANDOM()').first
     @popular_items = Item.all.reverse.take(4)
   end
+  
+  def create
+    render html: '<div>html goes here</div>' # if params[:commit] == "Subscribe"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get 'collections/kids'
   get 'collections/new_ins'
   resources :items
+  
+  # doesn't seem to work, want to post newsletter form submit in home/index to Home_Controller def create
+  post '/home/index', to: 'home#index'
   root 'home#index'
 end
