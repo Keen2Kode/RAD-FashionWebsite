4bbb7d2b9303addcb08d438eaeb7bc5c93c87f4c
Ubuntu <ubuntu@ip-172-31-5-178.us-east-2.compute.internal>
2021-05-17 01:53:47 +0000

    bag items restructured to variant_items and bag_items

---
+++ app/models/item_variant.rb
@@ -0,0 +1,16 @@
+class ItemVariant < ApplicationRecord
+    belongs_to :item
+    
+    validates :size,      presence: true
+    validates :colour,    presence: true
+    validate :no_matching_variant
+    
+    enum size: [:extra_small, :small, :medium, :large, :extra_large]
+    enum colour: [:blue, :green, :black, :brown, :red]
+    
+    def no_matching_variant
+        !item.item_variants.find_by(item_id: item.id, colour: colour, size: size)
+    end
+
+# validates unique colour size
+end
