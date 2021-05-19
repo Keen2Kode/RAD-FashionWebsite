5583c0e29f9b95d2844d4f0694956fe822e2d346
Jefferson Madrid <jefferson.imadrid@gmail.com>
2021-05-19 23:52:18 +1000

    Allow twitter login but not persisted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -149,6 +149,17 @@ class UsersController < ApplicationController
   
   
   
+  def create
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    session[:user_id] = @user.id
+    redirect_to root_path
+  end
+  
+  protected
+  
+  def auth_hash
+    request.env['omniauth.auth']
+  end
   
   
   
