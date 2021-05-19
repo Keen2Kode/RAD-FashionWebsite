fb22d78c0b0f99ec87c079d5d7b868201bde856a
Ubuntu <ubuntu@ip-172-31-5-178.us-east-2.compute.internal>
2021-05-19 18:08:13 +0000

    password reset works on development too

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,4 @@
 class UserMailer < ApplicationMailer
-# need to set up default_url_options = {host: "url.com"}
-# but it changes depending on who uses it
-# this is why ActionController has default_url_options method to dynamically set the email domain
-
-  # def default_url_options
-  #   { host: "https://257e557ba36343949ed1801d78c2d064.vfs.cloud9.us-east-2.amazonaws.com" }
-  # end
   
   def reset_password(request)
     @user = params[:user]
@@ -15,8 +8,10 @@ class UserMailer < ApplicationMailer
     # so a hacker can't send to a different email, check your global id, and change it
     # alternative to storing an authentication key for the user (Tute ll)
     token = @user.to_signed_global_id(purpose: "reset_password", expires_in: 30.minutes)
-    # @url  = reset_password_url(token: @reset_token, host: request.host_with_port, protocol: 'https')
-    @url = reset_password_url(token: token)
+    cloud9_url = reset_password_url(token: token)
+    heroku_url = reset_password_url(token: token, host: request.host_with_port, protocol: 'https')
+    
+    @url =  Rails.env.production? ? cloud9_url : heroku_url
     mail to: @user.email
   end
   
