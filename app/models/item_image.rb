c4b61b9c685431ed878026da6e4e8b9d2f8cef28
Ubuntu <ubuntu@ip-172-31-6-13.us-east-2.compute.internal>
2021-05-22 11:06:07 +0000

    item image and tag item test complete

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,3 +1,5 @@
 class ItemImage < ApplicationRecord
   belongs_to :item
+  
+  validates :image_link, presence: true
 end
--- test/models/item_image_test.rb
+++ test/models/item_image_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ItemImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @item_image = item_images(:one)
+  end
+  
+  test "should be valid" do
+    assert @item_image.valid?
+  end
+  
+  test "item image should not be nil" do
+    @item_image.image_link = nil
+    assert_not @item_image.valid?
+  end
+  
+  test "image should be present" do
+    @item_image.image_link = " "
+    assert_not @item_image.valid?
+  end
+  
+  test "image should allow duplicates" do
+    assert @item_image.dup.valid?
+  end
 end
