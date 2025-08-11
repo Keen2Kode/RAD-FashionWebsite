3fbc0f6f3dbfdd430f9d5e591a539c7b2e737ea3
Keen2Kode <70382344+Keen2Kode@users.noreply.github.com>
2025-08-11 22:29:56 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# this is normally run as a post deploy ssh shell command, but render doesn't support that, so we put it here.
+bundle exec rails db:migrate
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
