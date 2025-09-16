53570673276796ae8db16b8d83dc659cb170bb60
Reuben <70382344+Keen2Kode@users.noreply.github.com>
2025-09-17 00:34:46 +1000

    simplify entrypoint to create and seed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-# this is normally run as a post deploy ssh shell command, but render doesn't support that, so we put it here.
-# f
-bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:setup
+# don't need persistent data, so just create and load the database on each startup
+bundle exec rails db:setup
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
